535e11b338f87fba78890558eba99595b16ce7e1
OM <omjaiswal2552@gmail.com>
2026-01-24 13:23:29 +0530

    added script.js

---
+++ portfolio/script.js
@@ -0,0 +1,37 @@
+// console.log('JavaSrcipt is connected');
+
+let num1 = 5;
+// let num2 = 10;
+
+
+//  condition in js
+// if (num1 < num2) {
+//     console.log(`${num1} is smaller than ${num2}`)
+// } else {
+//     console.log(`${num1} is greater than ${num2}`)
+// }
+
+
+// // for loop in js
+// for (let i=0; i<num1; i++) {
+//     console.log(i);
+// }
+
+let arr1 = [2, 4, 6, 8]
+console.log(arr1);
+
+// push new entry at the end
+arr1.push(10);
+console.log(arr1);
+
+// pop one element from end
+arr1.pop();
+console.log(arr1);
+
+// add new entry in the beginning 
+arr1.shift();
+console.log(arr1);
+
+// remove first element from array
+arr1.unshift(5);
+console.log(arr1);
