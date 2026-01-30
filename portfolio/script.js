7b116de001d934490730305a22513bf9ed8e651c
OM <omjaiswal2552@gmail.com>
2026-01-30 22:27:57 +0530

    add events and validations

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,58 +1,89 @@
-console.log('JavaSrcipt is connected');
+const form = document.querySelector('#form');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const message = document.querySelector('#message');
+const error = document.querySelector('#error');
 
-let num1 = 5;
-let num2 = 10;
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-//  condition in js
-if (num1 < num2) {
-    console.log(`${num1} is smaller than ${num2}`)
-} else {
-    console.log(`${num1} is greater than ${num2}`)
-}
+    if (!name.value.trim()){
+        // alert('Name is required!');
+        error.innerText = 'Name is required!'
+    }
+    else if (name.value.length < 4) {
+        error.innerText = 'Name should be at least of four letters'
+    }
 
+    else if (!email.value.trim()) {
+        alert('Email is required!');
+    }
 
-// for loop in js
-for (let i=0; i<num1; i++) {
-    console.log(i);
-}
+    else if (!message.value.trim()) {
+        alert('You must add message to send it!');
+    }
 
-let arr1 = [2, 4, 6, 8]
-console.log(arr1);
-
-// push new entry at the end
-arr1.push(10);
-console.log(arr1);
-
-// pop one element from end
-arr1.pop();
-console.log(arr1);
-
-// remove first element from array
-arr1.shift();
-console.log(arr1);
-
-// add new entry in the beginning 
-arr1.unshift(5);
-console.log(arr1);
+    else {
+        alert('Form submitted successfully');
+    }
+})
 
 
-// Object in JavaScript  - Object stores data as key-value pairs
-const student = {
-    name: 'Ram',
-    age: 25,
-    marks: 80
-}
+// console.log('JavaSrcipt is connected');
 
-console.log(`Name: ${student.name}, Age: ${student.age}, Marks: ${student.marks}`);
+// let num1 = 5;
+// let num2 = 10;
 
-const students = [
-    { Name: 'Ram', Age: 25, Marks: 80 },
-    { Name: 'Mohan', Age: 24, Marks: 82},
-    { Name: 'Radha', Age: 23, Marks: 85},
-    { Name: 'Sita', Age: 24, Marks: 81}
-]
+// //  condition in js
+// if (num1 < num2) {
+//     console.log(`${num1} is smaller than ${num2}`)
+// } else {
+//     console.log(`${num1} is greater than ${num2}`)
+// }
 
-// print data from students using for loop.
-for (let i=0; i<students.length; i++) {
-    console.log(`Name: ${students[i].Name}, Age: ${students[i].Age}, Marks: ${students[i].Marks} `)
-}
+
+// // for loop in js
+// for (let i=0; i<num1; i++) {
+//     console.log(i);
+// }
+
+// let arr1 = [2, 4, 6, 8]
+// console.log(arr1);
+
+// // push new entry at the end
+// arr1.push(10);
+// console.log(arr1);
+
+// // pop one element from end
+// arr1.pop();
+// console.log(arr1);
+
+// // remove first element from array
+// arr1.shift();
+// console.log(arr1);
+
+// // add new entry in the beginning 
+// arr1.unshift(5);
+// console.log(arr1);
+
+
+// // Object in JavaScript  - Object stores data as key-value pairs
+// const student = {
+//     name: 'Ram',
+//     age: 25,
+//     marks: 80
+// }
+
+// console.log(`Name: ${student.name}, Age: ${student.age}, Marks: ${student.marks}`);
+
+// const students = [
+//     { Name: 'Ram', Age: 25, Marks: 80 },
+//     { Name: 'Mohan', Age: 24, Marks: 82},
+//     { Name: 'Radha', Age: 23, Marks: 85},
+//     { Name: 'Sita', Age: 24, Marks: 81}
+// ]
+
+// // print data from students using for loop.
+// for (let i=0; i<students.length; i++) {
+//     console.log(`Name: ${students[i].Name}, Age: ${students[i].Age}, Marks: ${students[i].Marks} `)
+// }
